dcd4c0619d9c24a384ec2900b5ba1e72323154dc
mhrSujan <sujanmaharjan054@gmail.com>
2026-06-28 20:24:03 -0400

    Day 1 of learning python

---
+++ phase1-python/day1.py
@@ -0,0 +1,57 @@
+#print("hello python!")
+
+
+
+#print("Name:Sujan Maharjan\nAge:26\nCountry:United States of America")
+
+
+
+#input practice
+#name = input("What is your name?)
+#print("Hello!", name)
+
+
+
+#Calculator practice
+# a = float(input("Enter first number: "))
+# b = float(input("Enter second number: "))
+# sum = a + b
+# print("The sum of", a, "and", b, "is", sum)
+
+
+
+#A minimal form type practice
+# name = input("What is your name :  ")
+# age = input("What is your age :  ")
+# place = input("Where do you live :  ")
+# #print("Hello!", "My name is", name, "\n", "I am", age, "years old", "\n", "I live in", place)
+# print("In 10 years, I will be", int(age) + 10, "years old")
+
+
+
+# Rectangle Area and Perimeter Calculator
+# length = float(input("Enter the length of the rectangle: "))
+# width = float(input("Enter the width of the rectangle: "))
+# area = length * width
+# perimeter = 2 * (length + width)
+# print("The area of the rectangle is:", area)
+# print("The perimeter of the rectangle is:", perimeter)
+
+
+
+#Defining a function to square a number
+# def square(n):
+#     return n ** 2
+
+# def main():
+#     x=float(input("Enter a number to square: "))
+#     result = square(x)
+#     print("The square of", x, "is", result)
+
+
+
+# Creating a simple BMI Calculator
+# height = float(input("Enter your height in meters: "))
+# weight = float(input("Enter your weight in kilograms: "))
+# bmi = calculate_bmi(height, weight)
+# print("Your BMI is:", bmi)
