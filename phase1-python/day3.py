b40c51973c46393be4c3bbf6f80d48d75fe2f22b
mhrSujan <sujanmaharjan054@gmail.com>
2026-07-06 17:14:03 -0400

    day3 learning py

---
+++ phase1-python/day3.py
@@ -0,0 +1,18 @@
+#Importance of loops in programming
+
+#for and while
+
+# for i in range(5):
+#     print("This is iteration number:", i)
+
+# counter = 0
+# while counter < 5:
+#     print("This is iteration number:", counter)
+#     counter += 1
+
+#break and continue statements
+for i in range(10):
+    if i == 8:
+        break
+    print("This is iteration number:", i)
+
